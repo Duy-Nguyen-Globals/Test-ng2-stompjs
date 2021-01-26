81b0c7463acdd049b0902f65c52f54b8f7c46bb3
Nguyen-Duy-Globals <duy_nguyen@globals.jp>
2021-01-26 11:45:21 +0900

    no message

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { RxStompService } from '@stomp/ng2-stompjs';
 import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ng2-stompjs-angular7';
+  message: string;
+
+  constructor(
+    private rxStompService: RxStompService,
+    private http: HttpClient
+  ) {
+
+  }
+
+  ngOnInit() {
+    this.rxStompService.watch('/jre/socket-publisher/message-channel').subscribe((message) => {
+      console.log(message.body);
+      this.message = message.body;
+    })
+  }
+  
+  sendMessage() {
+    this.rxStompService.publish({
+      destination: '/jre/socket-subscriber/send/message', 
+      body: 'test'
+    });
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
+import { InjectableRxStompConfig, RxStompService, rxStompServiceFactory } from '@stomp/ng2-stompjs';
+import { rxStompConfig } from './my-rx-stomp.config';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: InjectableRxStompConfig,
+      useValue: rxStompConfig
+    },
+    {
+      provide: RxStompService,
+      useFactory: rxStompServiceFactory,
+      deps: [InjectableRxStompConfig]
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
